c31cc0de514905bd4dc60433db8d831fc7a4299a
Ethan Perkins <ethanperkinspro@gmail.com>
2021-11-10 14:19:57 -0700

    finished project

--- AboutMeBasic/ViewController.swift
+++ AboutMeBasic/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var bioLabel: UILabel!
+    
+ 
+    func setUpAboutMe() {
+            
+        nameLabel.text  = "Ethan Perkins"
+        titleLabel.text = "iOS Developer, car lover, gym rat"
+        ageLabel.text   = "Age: 20"
+        
+        profileImageView.image = UIImage(named: "ethan")
+        
+        bioLabel.text = "Bio: Ethan was born in Salt Lake City, Utah, in 2001.  He's grown up in various towns including Draper, Riverton, and Sandy.  He graduated Riverton highshool in 2019 and has been a self taught iOS Developer ever since, and is currently attending the immersive iOS course at DevMountain."
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpAboutMe()
     }
-
-
-}
+ }
 
